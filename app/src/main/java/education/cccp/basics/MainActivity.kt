7d32dd465045b5ae5616d029514e9d4e043f82e3
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-24 10:45:04 +0100

    ajout de methode illustrant les lifes cycle

--- app/src/main/java/education/cccp/basics/MainActivity.kt
+++ app/src/main/java/education/cccp/basics/MainActivity.kt
@@ -1,11 +1,58 @@
 package education.cccp.basics
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(MainActivity::class.java.name,"onCreate")
     }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(MainActivity::class.java.name,"onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(MainActivity::class.java.name,"onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(MainActivity::class.java.name,"onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(MainActivity::class.java.name,"onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(MainActivity::class.java.name,"onStop")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(MainActivity::class.java.name,"onStart")
+    }
+
+    fun onClickButtonEvent(view: View) {
+        var intent = Intent(
+            this,
+            SecondActivity::class.java
+        )
+        startActivity(intent)
+    }
+
+
 }
--- app/src/main/java/education/cccp/basics/SecondActivity.kt
+++ app/src/main/java/education/cccp/basics/SecondActivity.kt
@@ -2,10 +2,40 @@ package education.cccp.basics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(SecondActivity::class.java.name,"onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(SecondActivity::class.java.name,"onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(SecondActivity::class.java.name,"onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(SecondActivity::class.java.name,"onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(SecondActivity::class.java.name,"onStop")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(SecondActivity::class.java.name,"onStart")
     }
 }
